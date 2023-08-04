eb964764208e26b799418510f48daa791a66c46c
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-04 16:17:44 -0700

    new Page component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,19 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
+
 export default function Home() {
   return (
-    <main className="text-green-500">
-      Main Content
+    <main className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">
+            Welcome back
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mt-4">
+            <ListItem image="/liked.png" name="Liked songs" href="" />
+          </div>
+        </div>
+      </Header>
     </main>
   )
 }
