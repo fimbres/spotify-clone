3baf1f91d9f1ac41859eace7abb9efbf5ddf5334
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-04 23:38:11 -0700

    protecting route

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,10 +3,20 @@
 import React from 'react';
 import { TbPlaylist } from 'react-icons/tb';
 import { AiOutlinePlus } from 'react-icons/ai';
+import { useAuthModal } from '@/hooks/useAuthModal';
+import { useUser } from '@/hooks/useUser';
+import { useUploadModal } from '@/hooks/useUploadModal';
 
 const Library = () => {
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
     const handleClick = () => {
-        // handle upload
+        if(!user) {
+            authModal.onOpen();
+        }
+
+        uploadModal.onOpen();
     }
 
   return (
