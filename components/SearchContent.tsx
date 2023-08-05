e1dba39fecc60318175e0336d04df235ec07e5af
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-05 01:13:44 -0700

    new SearchContent component

---
+++ components/SearchContent.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Song } from '@/types';
+import React from 'react'
+import MediaItem from './MediaItem';
+
+interface SearchContentProps {
+    songs: Song[];
+}
+
+const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  if(songs.length === 0) {
+    return (
+        <div className='flex flex-col gap-y-2 w-full px-6 text-neutral-400' >
+            No songs found.
+        </div>
+    );
+  }
+  
+    return (
+    <div className='flex flex-col gap-y-2 w-full px-6'>
+        {songs.map(song => (
+            <div key={song.id} className='flex items-center gap-x-4 w-full'>
+                <MediaItem data={song} onClick={console.log}/>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default SearchContent
