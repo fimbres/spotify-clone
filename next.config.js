ac14d595b3ada3568c9c59c437f3f192da819d91
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-05 00:43:12 -0700

    adding new hostname

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            'zjunyuydvwwhzoalhete.supabase.co'
+        ]
+    }
+}
 
 module.exports = nextConfig
