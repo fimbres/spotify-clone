6f8a2c4147a3e1693801743024d1b6f34de68b13
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-05 00:43:33 -0700

    updating layout logic to show songs

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { SupabaseProvider } from '@/providers/SupabaseProvider'
 import { UserProvider } from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import { ToasterProvider } from '@/providers/ToasterProvider'
+import { getSongsByUserId } from '@/actions/get_songs_by_user_id'
 
 const inter = Figtree({ subsets: ['latin'] })
 
@@ -14,11 +15,14 @@ export const metadata: Metadata = {
   description: 'This is a realistic Spotify clone, made just with learning purposes.',
 }
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const userSongs = await getSongsByUserId();
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -26,7 +30,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <SideBar>
+            <SideBar songs={userSongs}>
               {children}
             </SideBar>
           </UserProvider>
