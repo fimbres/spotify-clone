c25cf409c7c4a8c8d5378e0722c75be650ce175a
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-07 16:38:28 -0700

    new Liked songs page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
             Welcome back
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mt-4">
-            <ListItem image="/liked.png" name="Liked songs" href="" />
+            <ListItem image="/liked.png" name="Liked songs" href="/liked" />
           </div>
         </div>
       </Header>
