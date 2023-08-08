20af4ca378ae85e88c278f24aca17c68304bc858
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-08 00:33:43 -0700

    new Account page

---
+++ app/(site)/account/page.tsx
@@ -0,0 +1,29 @@
+import AccountContent from '@/components/AccountContent';
+import Header from '@/components/Header';
+import React from 'react'
+
+const Account = () => {
+  return (
+    <div 
+      className="
+        bg-neutral-900 
+        rounded-lg 
+        h-full 
+        w-full 
+        overflow-hidden 
+        overflow-y-auto
+      "
+    >
+      <Header bgColor="bg-gradient-to-b from-neutral-700 to-transparent">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl font-semibold">
+            Account Settings
+          </h1>
+        </div>
+      </Header>
+      <AccountContent />
+    </div>
+  )
+}
+
+export default Account;
---
+++ components/AccountContent.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { useUser } from "@/hooks/useUser";
+import Button from "@/components/Button";
+import { useSubscribeModal } from "@/hooks/useSubscribe";
+import { postData } from "@/libs/helpers";
+
+const AccountContent = () => {
+  const router = useRouter();
+  const subscribeModal = useSubscribeModal();
+  const { isLoading, subscription, user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/');
+    }
+  }, [isLoading, user, router]);
+
+  const redirectToCustomerPortal = async () => {
+    setLoading(true);
+    try {
+      const { url, error } = await postData({
+        url: '/api/create-portal-link'
+      });
+
+      if(error) {
+        throw error;
+      }
+
+      window.location.assign(url);
+    } catch (error) {
+      if (error) return alert((error as Error).message);
+    }
+    setLoading(false);
+  };
+
+  return ( 
+    <div className="mb-7 px-6">
+      {!subscription && (
+        <div className="flex flex-col gap-y-4 max-w-[300px]">
+        <p>No active plan.</p>
+        <Button 
+          onClick={subscribeModal.onOpen}
+        >
+          Subscribe
+        </Button>
+      </div>
+      )}
+      {subscription && (
+        <div className="flex flex-col gap-y-4 max-w-[300px]">
+          <p>You are currently on the 
+            <b> {subscription?.prices?.products?.name} </b> 
+            plan.
+          </p>
+          <Button
+            disabled={loading || isLoading}
+            onClick={redirectToCustomerPortal}
+          >
+            Open customer portal
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
+ 
+export default AccountContent;
