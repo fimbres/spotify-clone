8cf9d1b8f5f213f014ea1973509b6be47e7ad461
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-07 17:04:51 -0700

    fixing Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,9 +15,10 @@ import { toast } from 'react-hot-toast';
 interface HeaderProps {
     children: React.ReactNode;
     className?: string;
+    bgColor?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ children, className }) => {
+const Header: React.FC<HeaderProps> = ({ children, className, bgColor }) => {
     const router = useRouter();
     const { onOpen } = useAuthModal();
     const supabaseClient = useSupabaseClient();
@@ -36,7 +37,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
     }
 
   return (
-    <div className={`h-fit bg-gradient-to-b from-emerald-800 to-transparent p-6 ${className}`}>
+    <div className={`h-fit p-6 ${className} ${bgColor ? bgColor : 'bg-gradient-to-b from-emerald-800 to-transparent'}`}>
         <div className='w-full mb-4 flex items-center justify-between'>
             <div className='hidden md:flex gap-x-2 items-center'>
                 <button className='rounded-full bg-black flex items-center justify-center hover:opacity-75 transition' onClick={() => router.back()}>
